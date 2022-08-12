083d423115484d72ef48a6594900d885bf388509
natnael-OQP <makiya112545@gmail.com>
2022-08-13 00:59:58 +0300

    Category Preview section created and change to state-full widget

--- lib/screens/partners/partners.dart
+++ lib/screens/partners/partners.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:reward_app/components/separator.dart';
+import 'package:reward_app/models/category.dart';
+import 'package:reward_app/screens/partners/components/category_button.dart';
 import 'package:reward_app/screens/partners/components/search_filed.dart';
 
-class PartnerScreen extends StatelessWidget {
+class PartnerScreen extends StatefulWidget {
   const PartnerScreen({super.key});
+
+  @override
+  State<PartnerScreen> createState() => _PartnerScreenState();
+}
+
+class _PartnerScreenState extends State<PartnerScreen> {
+  int checkedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +23,7 @@ class PartnerScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // title
           Padding(
@@ -48,7 +58,37 @@ class PartnerScreen extends StatelessWidget {
           const SearchFIled(),
           const Gap(25),
           const Separator(height: 25),
-          // Icons.
+          // category Preview
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Row(
+                children: List.generate(
+                  demo_categories.length,
+                  (index) => CategoryButton(
+                    checkedIndex: index == checkedIndex,
+                    icon: demo_categories[index].icon,
+                    onPressed: () {
+                      setState(() => checkedIndex = index);
+                    },
+                    title: demo_categories[index].title,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Gap(20),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 18.0),
+            child: Text(
+              "Top Offers",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
         ],
       ),
     );
