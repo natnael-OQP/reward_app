19373d73f4660b9d33c816ff44a6c2c130f1c79d
natnael-OQP <makiya112545@gmail.com>
2022-08-13 18:29:20 +0300

    partner model created

---
+++ lib/models/partners.dart
@@ -0,0 +1,39 @@
+class Partners {
+  final String image, title, subTitle, buttonText;
+
+  Partners({
+    required this.image,
+    required this.title,
+    required this.subTitle,
+    required this.buttonText,
+  });
+}
+
+List<Partners> demo_partners = [
+  Partners(
+    image:
+        "https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    title: "Uber Eats",
+    subTitle: "20% off",
+    buttonText: "REDEEM",
+  ),
+  Partners(
+    image:
+        "https://images.pexels.com/photos/1126993/pexels-photo-1126993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Zara 2022",
+    subTitle: "10% off",
+    buttonText: "ZARA",
+  ),
+  Partners(
+    image:
+        "https://images.unsplash.com/photo-1520263115673-610416f52ab6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG5pa2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    title: "NIke 2022",
+    subTitle: "40% off",
+    buttonText: "SPORT",
+  ),
+];
+
+// business
+// 
+// https://images.unsplash.com/photo-1557425493-6f90ae4659fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80
+// https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=884&q=80
