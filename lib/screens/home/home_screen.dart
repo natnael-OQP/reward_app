fb4621d29c7b9b8c6d33ea4b730d2ebad15768b5
natnael-OQP <makiya112545@gmail.com>
2022-08-14 22:37:49 +0300

    change AppBar to silverAppBar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:reward_app/screens/events/events_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,123 +10,130 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int currentIndex = 1;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
-      initialIndex: 1,
       child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          toolbarHeight: 100,
-          leadingWidth: 80,
-          backgroundColor: Colors.white,
+          body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              elevation: 0,
+              toolbarHeight: 100,
+              leadingWidth: 80,
+              backgroundColor: Colors.white,
+              pinned: true,
+              floating: true,
 
-          /// avatar
-          leading: const Padding(
-            padding: EdgeInsets.only(left: 10),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/81810944?v=4"),
-            ),
-          ),
-
-          /// title
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Hello Ryan!",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+              // avatar
+              leading: const Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/81810944?v=4"),
                 ),
               ),
-              const Gap(4),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  Icon(
-                    Icons.location_on,
-                    color: Colors.black45,
-                    size: 15,
-                  ),
-                  Text(
-                    "EcoWorld",
+
+              /// title
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Hello Ryan!",
                     style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const Gap(4),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.location_on,
+                        color: Colors.black45,
+                        size: 15,
+                      ),
+                      Text(
+                        "EcoWorld",
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              /// action
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.search,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+              bottom: TabBar(
+                isScrollable: true,
+                labelColor: Colors.blue,
+                indicatorColor: Colors.blue,
+                unselectedLabelColor: Colors.black54,
+                onTap: (index) => {setState(() => currentIndex = index)},
+                tabs: <Widget>[
+                  Tab(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.local_post_office_outlined,
+                          color:
+                              currentIndex == 0 ? Colors.blue : Colors.black54,
+                        ),
+                        const Gap(10),
+                        const Text("Posts")
+                      ],
+                    ),
+                  ),
+                  Tab(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.calendar_month,
+                          color:
+                              currentIndex == 1 ? Colors.blue : Colors.black54,
+                        ),
+                        const Gap(10),
+                        const Text("Events")
+                      ],
+                    ),
+                  ),
+                  Tab(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.people_outline,
+                          color:
+                              currentIndex == 2 ? Colors.blue : Colors.black54,
+                        ),
+                        const Gap(10),
+                        const Text("Connect")
+                      ],
                     ),
                   ),
                 ],
               ),
-              // search
-            ],
-          ),
-
-          /// action
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.search,
-                size: 30,
-                color: Colors.black,
-              ),
-            )
-          ],
-
-          bottom: TabBar(
-            labelColor: Colors.blue,
-            indicatorColor: Colors.blue,
-            unselectedLabelColor: Colors.black54,
-            onTap: (index) => {setState(() => currentIndex = index)},
-            tabs: <Widget>[
-              Tab(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.local_post_office_outlined,
-                      color: currentIndex == 0 ? Colors.blue : Colors.black54,
-                    ),
-                    const Gap(10),
-                    const Text("Posts")
-                  ],
-                ),
-              ),
-              Tab(
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.calendar_month,
-                      color: currentIndex == 1 ? Colors.blue : Colors.black54,
-                    ),
-                    const Gap(10),
-                    const Text("Events")
-                  ],
-                ),
-              ),
-              Tab(
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.people_outline,
-                      color: currentIndex == 2 ? Colors.blue : Colors.black54,
-                    ),
-                    const Gap(10),
-                    const Text("Connect")
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+            ),
+          ];
+        },
         body: TabBarView(
           children: [
             Center(
@@ -134,12 +142,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: Theme.of(context).textTheme.headline6,
               ),
             ),
-            Center(
-              child: Text(
-                "Events",
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ),
+            const EventsScreen(),
             Center(
               child: Text(
                 "Connect",
@@ -148,7 +151,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-      ),
+      )),
     );
   }
 }
