d9b91b27b74ef0965cff6b039ab9b781515bdd0d
natnael-OQP <makiya112545@gmail.com>
2022-08-14 18:53:46 +0300

    style top appBar section

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("HomeScreen")),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 100,
+        leadingWidth: 80,
+        backgroundColor: Colors.white,
+        leading: const Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://avatars.githubusercontent.com/u/81810944?v=4"),
+          ),
+        ),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Hello Ryan!",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const Gap(4),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.location_on,
+                  color: Colors.black45,
+                  size: 15,
+                ),
+                Text(
+                  "EcoWorld",
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+            // search
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+              size: 30,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
