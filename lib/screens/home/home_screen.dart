93c0241a9a674328973befcd97802599e628817a
natnael-OQP <makiya112545@gmail.com>
2022-08-14 19:37:44 +0300

    tabBar completed

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,68 +1,153 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: 100,
-        leadingWidth: 80,
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 1,
+      child: Scaffold(
         backgroundColor: Colors.white,
-        leading: const Padding(
-          padding: EdgeInsets.only(left: 10),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/81810944?v=4"),
+        appBar: AppBar(
+          elevation: 0,
+          toolbarHeight: 100,
+          leadingWidth: 80,
+          backgroundColor: Colors.white,
+
+          /// avatar
+          leading: const Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/81810944?v=4"),
+            ),
+          ),
+
+          /// title
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Hello Ryan!",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              const Gap(4),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.location_on,
+                    color: Colors.black45,
+                    size: 15,
+                  ),
+                  Text(
+                    "EcoWorld",
+                    style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              // search
+            ],
+          ),
+
+          /// action
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                size: 30,
+                color: Colors.black,
+              ),
+            )
+          ],
+
+          bottom: TabBar(
+            labelColor: Colors.blue,
+            indicatorColor: Colors.blue,
+            unselectedLabelColor: Colors.black54,
+            onTap: (index) => {setState(() => currentIndex = index)},
+            tabs: <Widget>[
+              Tab(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.local_post_office_outlined,
+                      color: currentIndex == 0 ? Colors.blue : Colors.black54,
+                    ),
+                    const Gap(10),
+                    const Text("Posts")
+                  ],
+                ),
+              ),
+              Tab(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.calendar_month,
+                      color: currentIndex == 1 ? Colors.blue : Colors.black54,
+                    ),
+                    const Gap(10),
+                    const Text("Events")
+                  ],
+                ),
+              ),
+              Tab(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.people_outline,
+                      color: currentIndex == 2 ? Colors.blue : Colors.black54,
+                    ),
+                    const Gap(10),
+                    const Text("Connect")
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: TabBarView(
           children: [
-            const Text(
-              "Hello Ryan!",
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+            Center(
+              child: Text(
+                "Posts",
+                style: Theme.of(context).textTheme.headline6,
               ),
             ),
-            const Gap(4),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                Icon(
-                  Icons.location_on,
-                  color: Colors.black45,
-                  size: 15,
-                ),
-                Text(
-                  "EcoWorld",
-                  style: TextStyle(
-                    color: Colors.black45,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
+            Center(
+              child: Text(
+                "Events",
+                style: Theme.of(context).textTheme.headline6,
+              ),
             ),
-            // search
+            Center(
+              child: Text(
+                "Connect",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
           ],
         ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.search,
-              size: 30,
-              color: Colors.black,
-            ),
-          )
-        ],
       ),
     );
   }
