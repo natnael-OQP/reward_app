6fb0cb5b7f989cdcb32d0fa58f11caff3de893c9
natnael-OQP <makiya112545@gmail.com>
2022-08-15 15:01:06 +0300

    change title and date

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -12,19 +12,19 @@ List<Events> demo_events = [
   Events(
     image:
         "https://images.unsplash.com/photo-1557425493-6f90ae4659fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-    title: "REDEEM",
-    date: "REDEEM",
+    title: "Business Head Meeting",
+    date: "Mon Jui 18, 12:00 pm",
   ),
   Events(
     image:
         " https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=884&q=80",
-    title: "ZARA",
-    date: "ZARA",
+    title: "Business Head Meeting",
+    date: "Mon Jui 18, 12:00 pm",
   ),
   Events(
     image:
         "https://images.unsplash.com/photo-1520263115673-610416f52ab6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG5pa2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    title: "SPORT",
-    date: "SPORT",
+    title: "Business Head Meeting",
+    date: "Mon Jui 18, 12:00 pm",
   ),
 ];
